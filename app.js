b53f7d33dc2a1832a3cf1730c30fa0cc31f0f5f6
Anirban Bhattacharjee <71592780+AnirbanB02@users.noreply.github.com>
2021-02-03 21:46:30 +0530

    signup validation(except DId)

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 const togglePass1 = document.querySelector("#togglePass1");
 const togglePass2 = document.querySelector("#togglePass2");
+const togglecPass2 = document.querySelector("#togglecPass2");
 const passwordone = document.querySelector("#Pass1");
 const password2 = document.querySelector("#Pass2");
+const cpassword2 = document.querySelector("#cPass2");
 
 sign_up_btn.addEventListener('click', () => {
   container.classList.add("sign-up-mode");
@@ -31,3 +33,56 @@ togglePass2.addEventListener('click', function (e) {
   // toggle the eye slash icon
   this.classList.toggle('fa-eye-slash');
 });
+
+togglecPass2.addEventListener('click', function (e) {
+  // alert("clicked");
+  // toggle the type attribute
+  const type = cpassword2.getAttribute('type') === 'password' ? 'text' : 'password';
+  cpassword2.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
+});
+
+//validation of signup form
+
+function seterror(id,error)
+{
+  document.getElementById(id).innerHTML = error;
+}
+
+function clearErrors(){
+  var errorspans = document.getElementsByClassName("errormsg");
+  for(let i =0; i< errorspans.length; i++){
+    errorspans[i].innerHTML = "";
+  }
+}
+
+function SignUpValidation(){
+  clearErrors();
+  var returnval = true;
+
+  var name = document.getElementById("fname").value;
+  var pass2 = document.getElementById('Pass2').value;
+  var cpass2 = document.getElementById('cPass2').value;
+  var Did = document.getElementById('fDid').value;
+
+  if(name.length <= 3){
+    seterror("nameerror","*Name too short");
+    returnval = false;
+  }
+
+  if(pass2.length < 8){
+    seterror("Pass2error","*Password must be atleast 8 haracters");
+    returnval = false;
+  }
+
+  if(cpass2 != pass2){
+    seterror("cPass2error","*Must match password");
+    returnval = false;
+  }
+
+
+
+
+  return returnval;
+}
