fd628829a671d42f84523647946bd8a7b928b8f4
Anirban Bhattacharjee <71592780+AnirbanB02@users.noreply.github.com>
2021-02-06 02:34:47 +0530

    timer funtion added

---
+++ otp.js
@@ -0,0 +1,18 @@
+var timeLeftDisplay = document.querySelector("#timeLeft");
+let timeleft = 600;  //10min
+
+//timer function
+function timer(){
+  setInterval(function(){
+    if(timeleft <= 0){
+      clearInterval(timeleft = 0);
+    }
+    let min = Math.floor(timeleft/60);
+    let sec = timeleft%60;
+    timeLeftDisplay.innerHTML = min + ":" + sec ;
+    timeleft -= 1;
+    
+  }, 1000);
+}
+
+timer();
